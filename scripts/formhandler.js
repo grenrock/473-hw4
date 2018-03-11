415757f7b93b7a3bf809447c7add35d814065e40
Steven Grenrock <stevengrenrock@Mac-mini.local>
2018-03-11 10:39:23 -0700

    scripts

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -31,6 +31,20 @@
     });
   };
 
+  FormHandler.prototype.addInputHandler = function (fn) {
+    console.log("Setting the input handler for form");
+    this.$formElement.on("input", "name='emailAddress']", function (event) {
+      var emailAddress = event.target.value;
+      var message = "";
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity("");
+      } else {
+        message = emailAddress + " is not an authrorized email address!";
+        event.target.setCustomValidity(message);
+      }
+    });
+  };
+
   App.FormHandler = FormHandler;
   window.App = App;
 
